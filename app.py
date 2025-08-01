9ebb965b2de04cade0cd8f74c0669f72f89a8551
Vishal <vishalpatil2212000@gmail.com>
2025-08-01 11:43:36 +0530

    Added courses file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
+
 app = Flask(__name__)
 app.secret_key = '123456'  # Required for session and flash messages
 
@@ -13,9 +14,54 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor(dictionary=True)
 
+import os
+from werkzeug.utils import secure_filename
+from datetime import datetime
+
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload-content', methods=['GET', 'POST'])
+def upload_material():
+    if session.get('role') not in ['admin']:
+        flash("Access denied!", "danger")
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        file = request.files['file']
+        uploaded_by = session['username']
+        upload_date = datetime.now()
+
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            cursor.execute("""
+                INSERT INTO materials (title, description, file_path, uploaded_by, upload_date)
+                VALUES (%s, %s, %s, %s, %s)
+            """, (title, description, filename, uploaded_by, upload_date))
+            conn.commit()
+
+            flash('Material uploaded successfully.', 'success')
+            return redirect(url_for('upload_material'))
+
+    return render_template('upload-content.html')
+
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/view_student')
+def view_student():
+    if session.get('role') != 'admin':
+        flash("Access denied!", "danger")
+        return redirect(url_for('login'))
+
+    cursor.execute("SELECT * FROM users WHERE role='student'")
+    students = cursor.fetchall()
+    return render_template('view_student.html', students=students)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -93,6 +139,18 @@ def instructor_dashboard():
         flash("Access denied!", "danger")
         return redirect(url_for('login'))
     return render_template("instructor_dashboard.html")
+
+@app.route('/courses')
+def courses():
+    if 'username' not in session:
+        flash("Please login first.", "warning")
+        return redirect(url_for('login'))
+
+    cursor.execute("SELECT * FROM materials")
+    materials = cursor.fetchall()
+    return render_template('courses.html', materials=materials)
+
+
 
 @app.route('/logout')
 def logout():
